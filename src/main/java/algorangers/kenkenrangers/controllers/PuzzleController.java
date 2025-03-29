1834a01357c77762bd00492ab63ba8037579bb86
LedgeA <lancegaerlan04@gmail.com>
2025-03-29 20:43:47 +0800

    Implemented some logic to Cage and Cell classes

--- src/main/java/algorangers/kenkenrangers/controllers/PuzzleController.java
+++ src/main/java/algorangers/kenkenrangers/controllers/PuzzleController.java
@@ -40,7 +40,7 @@ public class PuzzleController {
                 solutionSet[row][col] = 0;
             }
         }
-        
+
         return false;
     }
 
@@ -76,16 +76,95 @@ public class PuzzleController {
 
         Cell[] cells;
         char operator;
+        int result;
         int size;
 
         Cage() {
-            
+            generateRandomOperator();
+            this.cells = new Cell[size];
         }
-        
+
+        void generateRandomOperator() {
+            switch ((int)(Math.random() * 4) + 1) {
+                case 1 -> {
+                    this.operator = '+';
+                    this.size = (int)(Math.random() * 4) + 3;
+                }
+                case 2 -> {
+                    this.operator = '-';
+                    this.size = 2;
+                }
+                case 3 -> {
+                    this.operator = '*';
+                    this.size = 3;
+                }
+                case 4 -> {
+                    this.operator = '/';
+                    this.size = 2;
+                }
+            
+            }
+        }
+
+        void computeForResult() {
+            this.result = cells[0].value;
+
+            for (int i = 1; i < size; i++) {
+                this.result = performArithmetic(this.result, cells[i].value);
+            }
+        }
+
+        int performArithmetic(int operandOne, int operandTwo) {
+            switch (this.operator) {
+                case '+' -> {
+                    return operandOne + operandTwo;
+                }
+                case '-' -> {
+                    return operandOne - operandTwo;
+                }
+                case '*' -> {
+                    return operandOne * operandTwo;
+                }
+                case '/' -> {
+                    float result = operandOne / operandTwo;
+                    
+                    // return if the result is an integer
+                    if ((int) result == operandOne / operandTwo) {
+                        return operandOne / operandTwo;
+                    }
+
+                    this.operator = '-';
+
+                    return -1;
+                }
+            }
+
+            return -1;
+        }
+
+        void addCells(int row, int col, int value) {
+            for (int i = 0; i < size; i++) {
+                if (cells[i] == null) cells[i] = new Cell(row, col, value);
+            }
+        }
+
+        Cell getCell(int row, int col) {
+            for (int i = 0; i < size; i++) {
+                if (cells[i].row == row && cells[i].col == col) return cells[i];
+            }
+
+            return null;
+        }
     }
 
     static class Cell {
-        private int row, col, value;
+        int row, col, value;
+
+        Cell(int row, int col, int value) {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+        }
     }
 
 }
